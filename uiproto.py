94509c2a548d210e04d7cd7e939d4b5eb0a031c2
djamarasigan <152085544+djamarasigan@users.noreply.github.com>
2024-05-22 15:58:08 +0800

    Update uiproto.py

--- uiproto.py
+++ uiproto.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
 st.header('resi-bot')
-
-
+st.subheader('a multilingual receipt scanner')
+st.text('a protype made by Team Sentient)
+        
 st.file_uploader(
   'Upload Button',
   help='Upload here your receipts.'
