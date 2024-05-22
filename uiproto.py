24b9ef78bba67350657379feb62e4c3fa1af188f
djamarasigan <152085544+djamarasigan@users.noreply.github.com>
2024-05-22 18:51:39 +0800

    Update uiproto.py

--- uiproto.py
+++ uiproto.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+from openai import OpenAI
 
 #space
 st.write("")
@@ -24,11 +25,36 @@ expander2 = st.expander("AI Chat History")
 
 # Add content specific to each expander
 with expander1:
-  with st.chat_message("user"):
-    st.write("How may I assist you?")
+  st.title("ChatGPT-like clone")
 
-# Display a chat input widget at the bottom of the app.
-    st.chat_input("Say something")
-
+  client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+  
+  if "openai_model" not in st.session_state:
+      st.session_state["openai_model"] = "gpt-3.5-turbo"
+  
+  if "messages" not in st.session_state:
+      st.session_state.messages = []
+  
+  for message in st.session_state.messages:
+      with st.chat_message(message["role"]):
+          st.markdown(message["content"])
+  
+  if prompt := st.chat_input("What is up?"):
+      st.session_state.messages.append({"role": "user", "content": prompt})
+      with st.chat_message("user"):
+          st.markdown(prompt)
+  
+      with st.chat_message("assistant"):
+          stream = client.chat.completions.create(
+              model=st.session_state["openai_model"],
+              messages=[
+                  {"role": m["role"], "content": m["content"]}
+                  for m in st.session_state.messages
+              ],
+              stream=True,
+          )
+          response = st.write_stream(stream)
+      st.session_state.messages.append({"role": "assistant", "content": response})
+    
 with expander2:
   st.write("Data analysis and visualizations here.")
