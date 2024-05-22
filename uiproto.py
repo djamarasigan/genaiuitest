64fbee1244e975588c7897f10a10796fe3014b4b
djamarasigan <152085544+djamarasigan@users.noreply.github.com>
2024-05-22 16:20:40 +0800

    Update uiproto.py

--- uiproto.py
+++ uiproto.py
@@ -1,12 +1,21 @@
 import streamlit as st
-tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
-tab1.write("this is tab 1")
-tab2.write("this is tab 2")
 
-# You can also use "with" notation:
-with tab1:
-  st.radio('Select one:', [1, 2])
 
+# Create expanders with titles like tabs
+expander1 = st.expander("Tab 1: Introduction")
+expander2 = st.expander("Tab 2: Data Analysis")
+expander3 = st.expander("Tab 3: Conclusion")
+
+# Add content specific to each expander
+with expander1:
+    st.write("Content for the introduction section.")
+
+with expander2:
+    st.write("Data analysis and visualizations here.")
+
+with expander3:
+    st.write("Summary and concluding remarks.")
+  
 st.write("")
 
 st.header('resi-bot')
