2b534b7aafa043ee7ad3dc8964fb7cca42d93999
djamarasigan <152085544+djamarasigan@users.noreply.github.com>
2024-05-22 16:23:43 +0800

    Update uiproto.py

--- uiproto.py
+++ uiproto.py
@@ -1,5 +1,21 @@
 import streamlit as st
 
+#space
+st.write("")
+
+#headers
+st.header('resi-bot')
+st.subheader('a multilingual receipt scanner')
+st.text('a protype made by Team Sentient')
+
+#space
+st.write("")
+
+#upload button
+st.file_uploader(
+  'Upload Button',
+  help='Upload here your receipts.'
+)
 
 # Create expanders with titles like tabs
 expander1 = st.expander("Tab 1: Introduction")
@@ -15,16 +31,3 @@ with expander2:
 
 with expander3:
     st.write("Summary and concluding remarks.")
-  
-st.write("")
-
-st.header('resi-bot')
-st.subheader('a multilingual receipt scanner')
-st.text('a protype made by Team Sentient')
-
-st.write("")
-
-st.file_uploader(
-  'Upload Button',
-  help='Upload here your receipts.'
-)
