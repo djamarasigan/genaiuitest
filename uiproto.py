9a58911608a90b4d08a3add80fee6495e974fc67
djamarasigan <152085544+djamarasigan@users.noreply.github.com>
2024-05-22 16:15:53 +0800

    Update uiproto.py

--- uiproto.py
+++ uiproto.py
@@ -1,9 +1,17 @@
 import streamlit as st
 
+>>> tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+>>> tab1.write("this is tab 1")
+>>> tab2.write("this is tab 2")
+
+st.write("")
+
 st.header('resi-bot')
 st.subheader('a multilingual receipt scanner')
 st.text('a protype made by Team Sentient')
-        
+
+st.write("")
+
 st.file_uploader(
   'Upload Button',
   help='Upload here your receipts.'
