8c334ec223214b566307e884ff51e0f59d696e48
djamarasigan <152085544+djamarasigan@users.noreply.github.com>
2024-05-22 16:18:59 +0800

    Update uiproto.py

--- uiproto.py
+++ uiproto.py
@@ -4,6 +4,10 @@ import streamlit as st
 >>> tab1.write("this is tab 1")
 >>> tab2.write("this is tab 2")
 
+# You can also use "with" notation:
+>>> with tab1:
+>>>   st.radio('Select one:', [1, 2])
+
 st.write("")
 
 st.header('resi-bot')
