ceda0597a4ff79b7afa1359ecacf27ba3939ad63
djamarasigan <152085544+djamarasigan@users.noreply.github.com>
2024-05-23 00:36:52 +0800

    Update uiproto.py

--- uiproto.py
+++ uiproto.py
@@ -14,20 +14,24 @@ st.write("")
 #upload button
 st.file_uploader(
   'Upload Button',
-  help='Upload here your receipt.'
+  help='Supported file types: JPG, PNG'
 )
 
-# Create expanders with titles like tabs
+#tabs
 expander1 = st.expander("New AI Chat")
 expander2 = st.expander("AI Chat History")
 
 # Add content specific to each expander
 with expander1:
+  language = st.sidebar.selectbox("Language", ["English", "Filipino"])
   with st.chat_message("user"):
     st.write("Chat with AI for assistance.")
-    prompt = st.chat_input("Write here your problem.")
-    if prompt:
-      st.write(f"{prompt}")
+    if language:
+      freeform_text = st.sidebar.text_area(label="What is your question?",max_chars=100)
+    if freeform_text:
+      response = my_chatbot(language,freeform_text)
+      st.write(response['text'] )
+
     
 with expander2:
   st.write("You can see here your AI chat history.")
